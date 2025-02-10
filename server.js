f83552688ef5f019749fb15521be704c1d4407c3
nexelio321 <nexeliomarketing26@gmail.com>
2025-02-11 01:30:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,5 @@ app.post("/predict-mines", (req, res) => {
 
 // Start Server
 app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
